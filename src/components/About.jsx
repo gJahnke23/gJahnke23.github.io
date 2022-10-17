e763d3f549afc86e971a96d8ddaef3f1b72fb1f4
gJahnke23 <gannonjahnke@gmail.com>
2022-10-17 13:18:10 -0400

    up to 1:01

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,26 +2,26 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-        <div>
-            <div>
-                <h2>Trusted by developers across the world</h2>
-                <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores neque voluptates ut pariatur fugiat? Itaque quae corporis voluptate delectus ipsum.</p>
+    <div className='w-full my-32'>
+        <div className='max-w-[1240] mx-auto'>
+            <div className='text-center'>
+                <h2 className='text-5xl font-bold'>Trusted by developers across the world</h2>
+                <p className='text-3xl py-6'> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores neque voluptates ut pariatur fugiat? Itaque quae corporis voluptate delectus ipsum.</p>
             </div>
         </div>
 
-        <div>
-            <div>
-                <p>100%</p>
-                <p>Completion</p>
+        <div className='grid md:grid-cols-3 gap-1 px-2 text-center'>
+            <div className='border py-8 rounded-xl shadow-xl'>
+                <p className='text-6xl font-bold text-indigo-600'>100%</p>
+                <p className='text-gray-400 mt-2'>Completion</p>
             </div>
-            <div>
-                <p>24/7</p>
-                <p>Delivery</p>
+            <div className='border py-8 rounded-xl shadow-xl'>
+                <p className='text-6xl font-bold text-indigo-600'>24/7</p>
+                <p className='text-gray-400 mt-2'>Delivery</p>
             </div>
-            <div>
-                <p>100K</p>
-                <p>Transactions</p>
+            <div className='border py-8 rounded-xl shadow-xl'>
+                <p className='text-6xl font-bold text-indigo-600'>100K</p>
+                <p className='text-gray-400 mt-2'>Transactions</p>
             </div>
         </div>
     </div>
